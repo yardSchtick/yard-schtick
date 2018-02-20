d9f1a5b637c3e088b077cd7563d262864c0b93eb
bdlewis30 <32585973+bdlewis30@users.noreply.github.com>
2018-02-20 12:37:50 -0700

    Tested and updated sql files. Entered dummy info.

--- db/inventory_seed.sql
+++ db/inventory_seed.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS Inventory (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     inv_name VARCHAR(180),
     inv_picture VARCHAR(300),
     inv_desc VARCHAR(300),
-    inv_price VARCHAR(Decimal),
+    inv_price Decimal(10,2),
     sale_id INTEGER REFERENCES Sale (id)
 )
