a1947a3d32fd8ccc18ce655ede5d75354100bdef
capes3 <capener.cade@gmail.com>
2018-02-21 16:33:29 -0700

    Hardcoded the inventory list from the database in.  Cards are rendering.

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -7,60 +7,57 @@ import { GETURL } from '../Duck/redux';
 
 
 class InventoryList extends Component {
-    constructor(){
+
+    constructor() {
         super()
-        this.state={
-            inventory:null
-             }
-            }
-    
-    
-    componentWillMount(){
-        axios.get('/api/InventoryList/:id', {
-            params: {
-              id: 1
-            }
-          })
-                .then((response)=>{
-                    console.log(response)
-                //   this.setState(
-                //       {inventory:response.data}
-                //   )
-                }
+        this.state = {
+            inventory: null
+        }
+    }
+
+    componentWillMount() {
+        axios.get('/api/getInventory/1')
+            .then((response) => {
+                this.setState(
+                    { inventory: response.data }
                 )
-                .catch(function(error){
-                    console.log(error);
-                })
-                // console.log(this.props.match.url)
-                // this.props.GETURL(this.props.match.url)
             }
-    
-    
-    
-            render(){
-            //     console.log(this.state.sale)
-    
-            //     if (this.state.sale) {
-            //         var saleCard = this.state.sale.map((val, index)=>(
-                        
-            //              <div key={index}>
-            //                  <p>{val.sale_name}</p>
-            //                  <p>{val.sale_description}</p>
-            //                  <p>{val.start_time}</p>
-            //                  <p>{val.end_time}</p> 
-            //              </div>
-            //         ))
-            //     }
-                
-                return (
-                <div>
-                    This is the Inventory for a sale
-                    {/* {saleCard} */}
+            )
+            .catch(function (error) {
+                console.log(error);
+            })
+        console.log(this.props.match.url)
+        this.props.GETURL(this.props.match.url)
+    }
+
+    componentDidMount() {
+        this.props.GETURL(this.props.match.url)
+    }
+
+    render() {
+        console.log(this.state.inventory)
+
+        if (this.state.inventory) {
+            var InventoryCard = this.state.inventory.map((val, index) => (
+
+                <div key={index}>
+                    <p>{val.inv_name}</p>
+                    {/* <img src={val.inv_picture}/> */}
+                    <p>{val.inv_desc}</p>
+                    <p>{val.inv_price}</p>
                 </div>
-                )
-            // }
+            ))
+        }
+
+        return (
+            <div>
+                {InventoryCard}
+            </div>
+        )
     }
 }
 
-export default InventoryList;
+function mapStateToProps(state) { }
+
+export default connect(mapStateToProps, { GETURL })(InventoryList)
 
