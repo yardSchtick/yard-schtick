2e3ae5276362f7917cf08df53bde3a193375fcc1
Trent <mr.peschke@gmail.com>
2018-02-21 09:57:21 -0700

    working on footer

--- src/Duck/redux.js
+++ src/Duck/redux.js
@@ -1,22 +1,32 @@
 import axios from "axios";
 
 const initialState = {
-    user:{}
-
+    user:{},
+    url: '/mapview'
 }
 
 const DEMO ='DEMO'
+const GET_URL = 'GET_URL'
 
-export function getDemo(){
-    return{
-        type:DEMO,
-        payload: res.data
+// export function getDemo(){
+//     return{
+//         type:DEMO,
+//         payload: res.data
+//     }
+// }
+
+export function getURL(url) {
+    return {
+        type: GET_URL,
+        payload: url
     }
 }
-export default function reducer(state = initalState, action){
+export default function reducer(state = initialState, action){
     switch(action.type){
         case DEMO:
             return Object.assign( {}, state, { recipeToGet: action.payload } );
+        case GET_URL:
+            return Object.assign( {}, state, {url: action.payload})
         default:
             return state
     }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import {HashRouter} from 'react-router-dom';
 
 
-
-
 ReactDOM.render(
 <HashRouter>
 <App />
