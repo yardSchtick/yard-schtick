7d1707ad8ee15d4000155ff649a9e2486a3a077d
Ben Lewis <benlewis@Bens-MacBook-Pro.local>
2018-02-22 16:47:43 -0700

    set up auth0

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const express = require('express')
     , dotenv = require('dotenv').config()
     , massive = require('massive')
     , session = require('express-session')
+    , passport = require('passport')
+    , Auth0Strategy = require('passport-auth0');
 
 const ctrl = require('./controller.js')
 
@@ -16,6 +18,78 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
+
+// ========== AUTH0 ===============================\\
+const {
+    AUTH_DOMAIN,
+    AUTH_CLIENT_ID,
+    AUTH_CLIENT_SECRET,
+    AUTH_CALLBACK_URL,
+    CONNECTION_STRING
+} = process.env
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Auth0Strategy({
+    domain: AUTH_DOMAIN,
+    clientID: AUTH_CLIENT_ID,
+    clientSecret: AUTH_CLIENT_SECRET,
+    callbackURL: AUTH_CALLBACK,
+    scope: 'openid profile'
+}, function (accessToken, refreshToken, extraParams, profile, done) {
+    console.log(profile);
+    let { name, email, picture, user_id } = profile;
+    const db = app.get('db');
+    db.users.find_user([id]).then(function (user) {
+        if (!user[0]) {
+            db.create_user([
+                address_street,
+                address_city,
+                address_state,
+                address_zip,
+                user_name,
+                user_img
+            ]).then(user => {
+                return done(null, user[0].id)
+            })
+        }
+        else {
+            return done(null, user[0].id)
+        }
+    })
+}))
+
+passport.serializeUser((id, done) => {
+    return done(null, id)
+})
+
+passport.deserializeUser((id, done) => {
+    app.get('db').find_session_user([id])
+        .then(function (user) {
+            return done(null, user[0])
+        })
+})
+
+app.get('/auth', passport.authenticate('auth0'))
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: process.env.SUCCESSREDIRECT,
+    failureRedirect: process.env.FAILUREREDIRECT
+}))
+
+app.get('/auth/me', (req, res) => {
+    if (!req.user) {
+        res.status(404).send('User not found.');
+    }
+    else {
+        res.status(200).send(req.user);
+    }
+})
+
+app.get('auth/logout', function (req, res) {
+    req.logOut();
+    res.redirect('/')
+})
 
 // ===========NONE AUTH REQUIRED ENDPOINTS==========\\
 
