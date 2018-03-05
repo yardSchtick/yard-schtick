76837513eb0845602bae20ec30eccbcb0d6d3066
Trent <mr.peschke@gmail.com>
2018-03-05 10:21:42 -0700

    whatever

--- src/components/EditThankYou.js
+++ src/components/EditThankYou.js
@@ -13,16 +13,9 @@ class EditThankYou extends Component {
     return (
       <div className="thankYouContainer">
         <h1 className="title" id="thankYouTitle">Your sale has been edited.</h1>
-<<<<<<< HEAD
         <p className="subtitle" id="thankYouSubtitle">You can always</p>
         <Link to='/AddInventory'><button id="profileAddSaleButton">Add Items to Your Sale</button></Link>
         <p className="subtitle" id="thankYouSubtitle">if you'd like.</p>
-=======
-        <i className="glyphicon glyphicon-ok-sign"></i>
-        <p className="subtitle" id="thankYouSubtitle">Would you like to </p>
-        <Link to='/AddInventory'><button id="profileAddSaleButton">Add Items to Your Sale</button></Link>
-        <p className="subtitle" id="thankYouSubtitle2">to help people focus in on your sale?</p>
->>>>>>> master
         <div className="thankYouInner">
           <Link to="/ViewMap"><button className="thankYouButton">View Sales</button></Link>
           <Link to="/ProfileView"><button className="thankYouButton">View Profile</button></Link>
